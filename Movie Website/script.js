bfba4a7fdcb2e8b11e53247d9ced4d3cb53f4a12
Dheeraj1324 <sch19429@gmail.com>
2024-08-19 13:41:42 +0530

    visually Engaging Landing Page

---
+++ Movie Website/script.js
@@ -0,0 +1,25 @@
+function on(){
+    document.getElementById("ban").style.backgroundImage="url('pictures/duneback.webp')";
+    document.getElementById("title").innerHTML="DUNE";
+    document.getElementById("about").innerText="Dune, science fiction novel by American author Frank Herbert, serialized in Analog from 1963 to 1965 and then published in book form later in 1965. Dune follows young nobleman Paul Atreides through adversity to his destiny as a messianic leader on the arid desert planet Arrakis.";
+}
+function to(){
+    document.getElementById("ban").style.backgroundImage="url('pictures/anyback.webp')";
+    document.getElementById("title").innerHTML="ANYONE BUT YOU";
+    document.getElementById("about").innerText="In the edgy comedy Anyone But You, Bea (Sydney Sweeney) and Ben (Glen Powell) look like the perfect couple, but after an amazing first date something happens that turns their fiery hot attraction ice cold -- until they find themselves unexpectedly thrust together at a destination wedding in Australia.";
+}
+function th(){
+    document.getElementById("ban").style.backgroundImage="url('pictures/damback.jpg')";
+    document.getElementById("title").innerHTML="DAMSEL";
+    document.getElementById("about").innerText="A young woman, Elodie, agrees to marry a prince from a far away land to save her people from death. She soon discovers this fairytale is too good to be true when she is thrown into a cave with a dragon as a sacrifice. She must find a way to survive and escape using only her wit and bravery.";
+}
+function fo(){
+    document.getElementById("ban").style.backgroundImage="url('pictures/kungback.jpg')";
+    document.getElementById("title").innerHTML="KUNG FU PANDA 4";
+    document.getElementById("about").innerText="Po must train a new warrior when he's chosen to become the spiritual leader of the Valley of Peace. However, when a powerful shape-shifting sorceress sets her eyes on his Staff of Wisdom, he suddenly realizes he's going to need some help. Teaming up with a quick-witted corsac fox, Po soon discovers that heroes can be found in the most unexpected places.";
+}
+function fi(){
+    document.getElementById("ban").style.backgroundImage="url('pictures/aquaback.jpg')";
+    document.getElementById("title").innerHTML="AQUAMAN 2";
+    document.getElementById("about").innerText="After failing to defeat Aquaman the first time, Black Manta wields the power of the mythic Black Trident to unleash an ancient and malevolent force. Hoping to end his reign of terror, Aquaman forges an unlikely alliance with his brother, Orm, the former king of Atlantis. Setting aside their differences, they join forces to protect their kingdom and save the world from irreversible destruction.";
+}
