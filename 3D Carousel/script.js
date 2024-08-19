cfd9b2c962c6ada0dac550edf547756c5d1bb17a
Dheeraj1324 <sch19429@gmail.com>
2024-08-19 11:54:33 +0530

    Visual 3D Perspective of Images

---
+++ 3D Carousel/script.js
@@ -0,0 +1,33 @@
+function tan(){
+    document.getElementById("nme").innerHTML="RENGOKU<br>. . . . (Demon Slayer)";
+    document.getElementById("tanji").style.transform="translatex(350px) rotatey(75deg)";
+    document.getElementById("rengoku").style.transform="rotate(0deg) scale(1.3)";
+    document.getElementById("rengoku").style.filter="grayscale(0%)";
+
+    
+    
+  }
+  function ren(){
+    document.getElementById("rengoku").style.transform="translatex(325px) rotatey(75deg)";
+    document.getElementById("naruto").style.transform="rotate(0deg) scale(1.3)";
+    document.getElementById("naruto").style.filter="grayscale(0%)";
+    document.getElementById("nme").innerHTML="NARUTO UZUMAKI <br> . . . . (Naruto)";
+  }
+  function nar(){
+    document.getElementById("naruto").style.transform="translatex(300px) rotatey(75deg)";
+    document.getElementById("nanami").style.transform="rotate(0deg) scale(1.3)";
+    document.getElementById("nanami").style.filter="grayscale(0%)";
+    document.getElementById("nme").innerHTML="NANAMI<br>. . . . (Jujutsu kaisen)";
+  }
+  function nan(){
+    document.getElementById("nanami").style.transform="translatex(275px) rotatey(75deg)";
+    document.getElementById("baki").style.transform="rotate(0deg) scale(1.3)";
+    document.getElementById("baki").style.filter="grayscale(0%)";
+    document.getElementById("nme").innerHTML="BAKI HANMA <br> . . . (Baki The Grappler)";
+  }
+  function bak(){
+    document.getElementById("baki").style.transform="translatex(250px) rotatey(75deg)";
+    document.getElementById("tanji").style.transform="rotatey(0deg) translatex(0px) ";
+    document.getElementById("nme").innerHTML="TANJIRO KAMADO <br> . . . . (Demon Slayer)";
+  }
+  
